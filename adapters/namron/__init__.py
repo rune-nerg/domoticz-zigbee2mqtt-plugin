8c795b4955c7d68b4444ddc3c67adf958b9e6e82
RuneNerg <runenerg@gmail.com>
2020-09-01 00:33:15 +0200

    Eco-Dim.07 as DimmableBulbAdapter

--- adapters/namron/__init__.py
+++ adapters/namron/__init__.py
@@ -1,7 +1,13 @@
 from adapters.namron.namron4512702 import Namron4512702
 from adapters.namron.namron4512703 import Namron4512703
+from adapters.dimmable_bulb_adapter import DimmableBulbAdapter
 
 namron_adapters = {
-    '4512702': Namron4512702,      # Namron ZigBee 1 channel switch K4
-    '4512703': Namron4512703,      # Namron ZigBee 4 channel switch K8
+    '4512702': Namron4512702,           # Namron ZigBee 1 channel switch K4
+    '4512703': Namron4512703,           # Namron ZigBee 4 channel switch K8
+    'Eco-Dim.07': DimmableBulbAdapter,  # Namron push/turn ZigBee dimmer
 }
+
+
+# {"state":"OFF","linkquality":47,"brightness":20}
+# {"state":"ON","linkquality":47,"brightness":100}
