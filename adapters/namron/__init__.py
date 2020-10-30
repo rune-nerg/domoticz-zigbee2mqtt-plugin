ff444dec33ddd7e9e7eca2d8c37ac950a5028956
RuneNerg <runenerg@gmail.com>
2020-10-30 12:53:02 +0100

    Comment

--- adapters/namron/__init__.py
+++ adapters/namron/__init__.py
@@ -5,6 +5,6 @@ from adapters.dimmable_bulb_adapter import DimmableBulbAdapter
 namron_adapters = {
     '4512702': Namron4512702,           # Namron ZigBee 1 channel switch K4
     '4512703': Namron4512703,           # Namron ZigBee 4 channel switch K8
-    'Eco-Dim.07': DimmableBulbAdapter,  # Namron/EcoDim push/turn ZigBee dimmer
     # Namron sells this as a pure ZigBee dimmer. It also exists from other Vendors (EcoDim among others) as combined ZigBee/Z-Wave dimmer
+    'Eco-Dim.07': DimmableBulbAdapter,  # Namron/EcoDim push/turn ZigBee dimmer
 }
